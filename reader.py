856547f9bd3492b62ddca2766a1d5281a2cb18af
mckaradachi <markokapranov11@gmail.com>
2024-11-12 13:32:36 +0200

    improved depicting of dict

--- reader.py
+++ reader.py
@@ -5,6 +5,7 @@ with open("stud_dict.pkl", "rb") as file:
   for i in stud_dict:
       print(f"{i} grade:")
       for b in stud_dict[i]:
-          print(f"\t {b}:")
-          print(*stud_dict[i][b], sep='\n' + '\t')
+          print(f" {b}:  \n")
+          print(*stud_dict[i][b], sep='\n' + '')
+          print()
       print()
